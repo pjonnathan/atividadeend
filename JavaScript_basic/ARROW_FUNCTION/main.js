33fa8d6c7842da7cd4593bdf963a5e1c57216032
Pedro Jonnathan <jonnathanmatos846@gmail.com>
2023-08-31 15:00:20 -0300

    add commit

---
+++ JavaScript_basic/ARROW_FUNCTION/main.js
@@ -0,0 +1,3 @@
+let resultadoDaSoma = (numero1, numero2) => numero1 + numero2
+
+console.log(resultadoDaSoma(20,23))
---
+++ JavaScript_basic/FUNTION_ANONMAS/main.js
@@ -0,0 +1,5 @@
+let ResultadoDaSoma = function(numero1 , numero2) {
+    return numero1 + numero2;
+}
+
+console.log(ResultadoDaSoma(4,6))
