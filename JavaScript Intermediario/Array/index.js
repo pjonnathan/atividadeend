6984cc6f1f90ad107aec45e237c7a0a2ec8f7e0f
Pedro Jonnathan <jonnathanmatos846@gmail.com>
2023-10-23 20:34:04 -0300

    pratic array

---
+++ JavaScript Intermediario/Array/index.js
@@ -0,0 +1,9 @@
+let array = [1, "joga e joga", 0.5]
+
+console.log(array)
+
+
+let multdimencional = [
+    ["Nome","idade","lugar"],
+    ["caio", 12, "frança"],
+]
--- JavaScript Intermediario/Higher order functions/index.js
+++ JavaScript Intermediario/Higher order functions/index.js
@@ -1,4 +1,19 @@
-var calcularAnodeNascimento = function(idade, mesNasciemnto, imprimir) {
-    const mesAtual = 2 
-    
+/* var calcularAnodeNascimento = function(idade, diaNascimento , mesNasciemnto, imprimir) {
+    const mesAtual = 2
+    const diaAtual = 6
+    let anodeNascimento = 2023 - idade
+    if(mesNasciemnto >= mesAtual) {
+        if(diaNascimento>diaAtual) anodeNascimento--
+    }
+    imprimir(anodeNascimento)
 }
+
+let imprimirAnodeNascimento = anodeNascimento => {
+    console.log('Seu ano de nascimento é: ' + anodeNascimento)
+}
+
+calcularAnodeNascimento(33, 30 , 2, imprimirAnodeNascimento) */
+
+let mult = (numero, multiplicado) => numero * multiplicado
+
+console.log(mult(2,2))
