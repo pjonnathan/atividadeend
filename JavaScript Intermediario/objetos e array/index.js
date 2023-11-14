a3b5b4aaa220cc018a16790476d3a473f14048d6
Pedro Jonnathan <jonnathanmatos846@gmail.com>
2023-11-14 20:24:27 -0300

    diferenca objeto e array

---
+++ JavaScript Intermediario/objetos e array/index.js
@@ -0,0 +1,13 @@
+let frutas = ['uva', 'laranja', 'coco']
+
+let videoGame = {
+    nome: 'Xbox',
+    valor: 3000
+}
+
+let videoGame2 = {
+    nome: 'Xbox360',
+    valor: 2000
+}
+
+let videoGames = [videoGame, videoGame2]
